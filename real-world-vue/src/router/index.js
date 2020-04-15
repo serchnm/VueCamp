da06fe8d627f1c1a215c2dc4849ac46ce143ce0d
Sergio Navarro <sergionavarro@MacBook-Pro.local>
2020-04-15 12:00:38 -0700

    vue add

---
+++ real-world-vue/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCreate from "../views/EventCreate.vue";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "event-list",
+      component: EventList
+    },
+    {
+      path: "/event/:id",
+      name: "event-show",
+      component: EventShow,
+      props: true
+    },
+    {
+      path: "/event/create",
+      name: "event-create",
+      component: EventCreate
+    }
+  ]
+});
---
+++ real-world-vue/src/views/EventList.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }">Event Show #1</router-link>
+  </div>
+</template>
