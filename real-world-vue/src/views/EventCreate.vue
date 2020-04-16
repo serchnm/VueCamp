1444a6a1b11da2ada0c4d81d10b9c09f47624787
Sergio Navarro <sergionavarro@MacBook-Pro.local>
2020-04-16 00:05:49 -0700

    state getters

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Create Events</h1>
+  <div>
+    <h1>Create an Event, {{user.name}}</h1>
+    <p>This Event was created by {{user.id}}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat"> {{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import {mapState} from "vuex";
+export default {
+  computed: 
+    mapState(["user", "categories"])
+  }
+</script>
+
+<style>
+
+</style>
--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -2,13 +2,13 @@
   <div>
     <h1>Event Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <BaseIcon/>
+    <BaseIcon />
   </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   components: {
