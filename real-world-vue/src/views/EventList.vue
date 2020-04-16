764317ab360c097e3b28c2543b8aedef0c865f13
Sergio Navarro <sergio.navarro2501@hotmail.com>
2020-04-15 18:10:06 -0700

    course complete

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }">Event Show #1</router-link>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon/>
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log("There is an error", error.response);
+      })
+
+  }
+}
+</script>
--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Event Show #{{ id }}</h1>
+  <h1>Event Show {{ event.title}}</h1>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      event: []
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+        .then(response => {
+          this.event = response.data
+        })
+        .catch(error => {
+          console.log("There was an error", error.response)
+          
+        })
+  }
 };
 </script>
